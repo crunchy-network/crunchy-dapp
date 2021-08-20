78067f300a4506fe5370694fe2c8f2797a405a87
Joshua Dechant <josh@codecrafting.org>
2021-08-20 15:33:38 -0400

    switch ipfs.io to fleek

--- src/utils/ipfs.js
+++ src/utils/ipfs.js
@@ -3,6 +3,7 @@ export default {
   transformUri (uri) {
     uri = uri.replace('ipfs://', 'https://ipfs.fleek.co/ipfs/');
     uri = uri.replace('https://gateway.pinata.cloud/ipfs/', 'https://ipfs.fleek.co/ipfs/');
+    uri = uri.replace('https://ipfs.io/ipfs/', 'https://ipfs.fleek.co/ipfs/');
     return uri;
   }
 
