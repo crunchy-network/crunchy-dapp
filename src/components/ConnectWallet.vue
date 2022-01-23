dfd3b56bb670c23ba562f952686fc2635229fef2
Melvin <melvinnnamani@gmail.com>
2022-01-23 23:39:18 +0100

    chore: Added disclaimer text

--- src/components/ConnectWallet.vue
+++ src/components/ConnectWallet.vue
@@ -11,6 +11,13 @@
     <el-row type="flex" justify="center">
       <el-button type="success" round plain @click="connectWallet"><i class="fad fa-wallet"></i> Connect Wallet</el-button>
     </el-row>
+
+    <div style="margin-top: 26px">
+      <h2 style="text-align: center; font-weight: 400 !important; font-size: 14px;">
+        <span style="font-weight: 500">Disclaimer:</span> Information provided does not take into account the amount of liquidity available. Please make sure
+        you check with the DEX you wish to trade at before making a trade.
+      </h2>
+    </div>
   </div>
 </template>
 
