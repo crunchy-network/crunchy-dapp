4228809086fb63efaec7b0a2a11a2013e221a13a
Melvin <melvinnnamani@gmail.com>
2022-02-05 20:37:35 +0100

    adjusted NFT col grid

--- src/components/NftWalletView.vue
+++ src/components/NftWalletView.vue
@@ -9,9 +9,9 @@
     </el-row>
 
     <el-row type="flex" align="stretch" :gutter="20">
-        <el-col v-for="(nft, index) in collection" :key="index" :xs="24" :sm="12" :md="8" :lg="6" :xl="4">
-          <nft-asset-card :art="nft.assets[0]" :count="nft.count" :icon="nft.icon" :name="nft.name" type="Coollection"></nft-asset-card>
-        </el-col>
+      <el-col v-for="(nft, index) in collection" :key="index" :span="4" >
+        <nft-asset-card :art="nft.assets[0]" :count="nft.count" :icon="nft.icon" :name="nft.name" type="Coollection"></nft-asset-card>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -24,6 +24,24 @@ export default {
   data() {
     return {
       collection: [
+        {
+          count: 100,
+          name: "CyberKidz Club",
+          icon: require("../assets/nfts/icon1.png"),
+          assets: [require("../assets/nfts/1.png")],
+        },
+        {
+          count: 1,
+          name: "Froggos",
+          icon: require("../assets/nfts/icon2.png"),
+          assets: [require("../assets/nfts/2.png")],
+        },
+        {
+          count: 1005,
+          name: "CyberKidz Club",
+          icon: require("../assets/nfts/icon3.png"),
+          assets: [require("../assets/nfts/3.png")],
+        },
         {
           count: 100,
           name: "CyberKidz Club",
