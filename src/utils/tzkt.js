9a08cc55a7d9798cbf0926d5918e86f916e84d2a
Joshua Dechant <josh@codecrafting.org>
2022-03-16 18:26:30 -0400

    Add WTZ recent transactions

--- src/utils/tzkt.js
+++ src/utils/tzkt.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { BigNumber } from "bignumber.js";
 
-const makeReqest = async (uri, params = {}) => {
+const makeRequest = async (uri, params = {}) => {
   return axios.get(`${process.env.VUE_APP_TZKT_API_URL}${uri}`, {
     params: { limit: 1000, ...params },
   });
 };
 
-const makeFutureReqest = async (uri, params = {}) => {
+const makeFutureRequest = async (uri, params = {}) => {
   return axios.get(`${process.env.VUE_APP_TZKT_API_FUTURE_URL}${uri}`, {
     params: { limit: 1000, ...params },
   });
@@ -15,23 +15,23 @@ const makeFutureReqest = async (uri, params = {}) => {
 
 export default {
   async getBigMapKeys(id) {
-    return makeReqest(`/v1/bigmaps/${id}/keys`);
+    return makeRequest(`/v1/bigmaps/${id}/keys`);
   },
 
   async getContractBigMap(address, name, params = {}) {
-    return makeReqest(`/v1/contracts/${address}/bigmaps/${name}`, params);
+    return makeRequest(`/v1/contracts/${address}/bigmaps/${name}`, params);
   },
 
   async getContractBigMapKeys(address, name, params = {}) {
-    return makeReqest(`/v1/contracts/${address}/bigmaps/${name}/keys`, params);
+    return makeRequest(`/v1/contracts/${address}/bigmaps/${name}/keys`, params);
   },
 
   async getContractStorage(address) {
-    return makeReqest(`/v1/contracts/${address}/storage`);
+    return makeRequest(`/v1/contracts/${address}/storage`);
   },
 
   async getTokenBalance(address, contract, tokenId) {
-    const res = await makeFutureReqest(`/v1/tokens/balances`, {
+    const res = await makeFutureRequest(`/v1/tokens/balances`, {
       select: "balance",
       account: address,
       "token.contract": contract,
@@ -42,5 +42,12 @@ export default {
       return new BigNumber(res.data[0]);
     }
     return new BigNumber(0);
+  },
+
+  async getTransactions(params = {}) {
+    return makeRequest("/v1/operations/transactions", {
+      "sort.desc": "level",
+      ...params,
+    });
   },
 };
