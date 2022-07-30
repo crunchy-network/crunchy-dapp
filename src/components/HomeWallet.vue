a0e4536663d2a7a5a1f55d7d45988bf7504f2a24
Melvin <melvinnnamani@gmail.com>
2022-07-31 00:23:06 +0100

    chore: update home wallet tracker

--- src/components/HomeWallet.vue
+++ src/components/HomeWallet.vue
@@ -4,12 +4,12 @@
       type="flex"
       style="flex-wrap: wrap; row-gap: 10px"
       justify="space-between"
-      :gutter="40"
+      :gutter="10"
     >
-      <el-col :xs="24" :md="24" :lg="7">
+      <el-col :xs="24" :md="12" :lg="5">
         <el-card v-loading="homeWallet.loading" class="top">
           <h2 style="font-weight: 600; font-size: 16px; color: #757679ff">
-            Net Worth
+            Portfolio Value
           </h2>
 
           <h2 style="font-weight: 600; font-size: 28px; margin-bottom: 0">
@@ -20,9 +20,35 @@
                     suffix: " ꜩ",
                     decimal: ".",
                     thousand: ",",
-                    precision: 4,
+                    precision: 2,
                   })
                 : vueNumberFormat(homeWallet.netWorthUsd, {
+                    prefix: "$",
+                    decimal: ".",
+                    thousand: ",",
+                    precision: 2,
+                  })
+            }}
+          </h2>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :md="12" :lg="5">
+        <el-card v-loading="homeWallet.loading" class="top">
+          <h2 style="font-weight: 600; font-size: 16px; color: #757679ff">
+            Staked Value
+          </h2>
+
+          <h2 style="font-weight: 600; font-size: 28px; margin-bottom: 0">
+            {{
+              !showUsd
+                ? vueNumberFormat(getStakedValues.xtz, {
+                    prefix: "",
+                    suffix: " ꜩ",
+                    decimal: ".",
+                    thousand: ",",
+                    precision: 2,
+                  })
+                : vueNumberFormat(getStakedValues.usd, {
                     prefix: "$",
                     decimal: ".",
                     thousand: ",",
@@ -37,46 +63,61 @@
         style="display: flex; justify-content: center; align-items: center"
         :xs="24"
         :md="24"
-        :lg="3"
+        :lg="1"
         class="divider"
       >
         <el-divider direction="vertical"></el-divider>
         <el-divider direction="horizontal"></el-divider>
       </el-col>
-      <el-col :xs="24" :md="12" :lg="7">
+      <el-col :xs="24" :md="12" :lg="5">
         <el-card v-loading="homeWallet.loading" class="top">
           <h2 style="font-weight: 600; font-size: 16px; color: #757679ff">
             CRUNCH Balance
           </h2>
-
-          <h2 style="font-weight: 600; font-size: 28px; margin-bottom: 0">
-            {{
-              vueNumberFormat(homeWallet.crunchBal, {
-                prefix: "",
-                decimal: ".",
-                thousand: ",",
-                precision: 4,
-              })
-            }}
-          </h2>
+          <el-row type="flex" justify="space-between">
+            <h2 style="font-weight: 600; font-size: 28px; margin-bottom: 0">
+              {{
+                vueNumberFormat(homeWallet.crunchBal, {
+                  prefix: "",
+                  decimal: ".",
+                  thousand: ",",
+                  precision: 2,
+                })
+              }}
+            </h2>
+            <el-avatar
+              src="https://ipfs.fleek.co/ipfs/bafybeienhhbxz53n3gtg7stjou2zs3lmhupahwovv2kxwh5uass3bc5xzq"
+              fit="cover"
+              shape="circle"
+              :size="40"
+            ></el-avatar>
+          </el-row>
         </el-card>
       </el-col>
-      <el-col :xs="24" :md="12" :lg="7">
+      <el-col :xs="24" :md="12" :lg="5">
         <el-card v-loading="homeWallet.loading" class="top">
           <h2 style="font-weight: 600; font-size: 16px; color: #757679ff">
             crDAO Balance
           </h2>
 
-          <h2 style="font-weight: 600; font-size: 28px; margin-bottom: 0">
-            {{
-              vueNumberFormat(homeWallet.crDaoBal, {
-                prefix: "",
-                decimal: ".",
-                thousand: ",",
-                precision: 4,
-              })
-            }}
-          </h2>
+          <el-row type="flex" justify="space-between">
+            <h2 style="font-weight: 600; font-size: 28px; margin-bottom: 0">
+              {{
+                vueNumberFormat(homeWallet.crDaoBal, {
+                  prefix: "",
+                  decimal: ".",
+                  thousand: ",",
+                  precision: 2,
+                })
+              }}
+            </h2>
+            <el-avatar
+              src="https://ipfs.fleek.co/ipfs/bafybeigulbzm5x72qtmckxqvd3ksk6q3vlklxjgpnvvnbcofgdp6qwu43u"
+              fit="cover"
+              shape="circle"
+              :size="40"
+            ></el-avatar>
+          </el-row>
         </el-card>
       </el-col>
     </el-row>
@@ -109,23 +150,27 @@
         >
           Staked
         </button>
-        <button
-          class="tab-text"
-          disabled
-          :style="isActiveTab('nfts')"
-          @click="setActiveTab('nfts')"
-        >
-          NFTs
-        </button>
 
-        <button
+        <a
           class="tab-text"
-          disabled
-          :style="isActiveTab('history')"
-          @click="setActiveTab('history')"
+          style="color: #555cff; text-decoration: none"
+          :href="`https://tzkt.io/${$route.params.walletAddress || getPkh}`"
+          target="_blank"
         >
           History
-        </button>
+          <svg
+            width="12"
+            height="13"
+            viewBox="0 0 12 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.85714 1.71408C6.85714 1.24077 7.24018 0.856934 7.71429 0.856934H11.1188C11.258 0.856934 11.3705 0.879969 11.4696 0.922023C11.5473 0.963541 11.6652 1.02488 11.7482 1.10604C11.7482 1.10738 11.7482 1.10872 11.7509 1.10979C11.917 1.2764 11.9759 1.4939 12 1.7114C12 1.71247 12 1.71327 12 1.71408V5.14265C12 5.61676 11.617 5.99979 11.1429 5.99979C10.6687 5.99979 10.2857 5.61676 10.2857 5.14265V3.78461L5.74821 8.31943C5.41339 8.65426 4.87232 8.65426 4.5375 8.31943C4.20268 7.98461 4.20268 7.44354 4.5375 7.10872L9.07232 2.57122H7.71429C7.24018 2.57122 6.85714 2.18738 6.85714 1.71408ZM0 3.42836C0 2.48149 0.767411 1.71408 1.71429 1.71408H4.28571C4.75982 1.71408 5.14286 2.09792 5.14286 2.57122C5.14286 3.04533 4.75982 3.42836 4.28571 3.42836H1.71429V11.1426H9.42857V8.57122C9.42857 8.09711 9.81161 7.71408 10.2857 7.71408C10.7598 7.71408 11.1429 8.09711 11.1429 8.57122V11.1426C11.1429 12.0882 10.3741 12.8569 9.42857 12.8569H1.71429C0.767411 12.8569 0 12.0882 0 11.1426V3.42836Z"
+              fill="#555CFF"
+            />
+          </svg>
+        </a>
       </div>
       <div class="grid-content" style="text-align: right">
         <el-switch
@@ -298,7 +343,7 @@ export default {
   },
   computed: {
     ...mapState(["homeWallet"]),
-    ...mapGetters(["getPkh", "getAssets"]),
+    ...mapGetters(["getPkh", "getAssets", "getStakedValues"]),
   },
   watch: {
     getPkh() {
@@ -333,7 +378,7 @@ export default {
     },
 
     setActiveTab(tab = "") {
-      if (["portfolio", "nfts", "staked", "history"].includes(tab)) {
+      if (["portfolio", "staked"].includes(tab)) {
         this.activeTab = tab;
       }
     },
@@ -436,6 +481,10 @@ export default {
     color: #191b1f66;
     cursor: not-allowed;
   }
+}
+
+.divider {
+  opacity: 0.26;
 }
 
 .divider .el-divider.el-divider--horizontal {
