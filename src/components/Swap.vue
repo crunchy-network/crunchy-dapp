459ee66f68ef83368ddd8f954639c82ea57198cd
Josh Dechant <josh@codecrafting.org>
2022-12-13 10:40:51 -0500

    prep for prod

--- src/components/Swap.vue
+++ src/components/Swap.vue
@@ -2,31 +2,6 @@
   <div>
     <div id="swap-router-wrapper">
       <NavMenu />
-      <div class="disclaimer-section">
-        <div class="beta-tag"><div class="beta-ribbon">BETA</div></div>
-        <el-tooltip
-          placement="bottom"
-          effect="light"
-          popper-class="disclaimer-tip"
-        >
-          <div slot="content">
-            Crunchy.network utilizes a list of public and private API endpoints
-            to pull price data. With this price data, users are allowed to
-            interact with decentralized exchanges (DEXs) within the Tezos
-            ecosystem. Crunchy.network does not own or operate these DEXs.
-            <br />
-            <br />
-            Crunchy.network is not responsible for DEX issues, displayed tokens,
-            price, API issues, exchange rates, and user trading decisions.
-            <br />
-            <br />
-            The use of this product is the sole responsibility of the user.
-          </div>
-
-          <span>Disclaimer <i class="el-icon-warning" /></span>
-        </el-tooltip>
-      </div>
-
       <SwapFormMain :tokenList="tokenList" />
       <SwapFormFoot :tokenList="tokenList" />
     </div>
@@ -66,33 +41,5 @@ export default {
   }
   max-width: 420px;
   margin: auto;
-
-  .disclaimer-section {
-    width: 100%;
-    text-align: center;
-    margin-bottom: 8px;
-  }
-}
-.beta-tag {
-  position: fixed;
-  z-index: -1;
-  border-bottom: 50px solid #f15d59;
-  border-left: 50px solid transparent;
-  border-right: 50px solid transparent;
-  height: 0;
-  left: -52px;
-  top: 108px;
-  transform: rotate(-45deg);
-  width: 125px;
-  .beta-ribbon {
-    margin-top: 5px;
-    font-weight: bold;
-    font-size: 24px;
-    color: #ffffff;
-  }
-}
-.disclaimer-tip {
-  width: 300px;
-  padding: 20px 16px;
 }
 </style>
