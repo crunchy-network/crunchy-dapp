359f5218c9e5d9cae64c9d9d19d0b37d60872782
Melvin <melvinnnamani@gmail.com>
2023-01-20 19:34:49 +0100

    chore: evenly space out items in token metrics card

--- src/components/TokenMetrics.vue
+++ src/components/TokenMetrics.vue
@@ -1,9 +1,17 @@
 <template>
   <el-card
+    id="token-metrics"
     v-loading="loading"
     shadow="always"
-    body-style="padding-bottom: 50px"
-    style="height: 100%"
+    body-style="padding-bottom: 50px; flex: 1;  display: flex;
+      flex-direction: column;
+      justify-content: stretch;"
+    style="
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: stretch;
+    "
   >
     <h3
       style="
@@ -18,18 +26,25 @@
     </h3>
     <el-row
       type="flex"
-      align="middle"
+      align="stretch"
       style="
         font-size: 14px;
         font-weight: 600;
         padding-bottom: 4px;
         width: 100%;
-        margin-top: auto;
+        flex: 1;
       "
     >
       <el-col :span="24">
-        <el-row type="flex" style="color: #757679">
-          <el-col :span="11">
+        <el-row type="flex" style="color: #757679; height: 100%">
+          <el-col
+            style="
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+            "
+            :span="11"
+          >
             <el-col
               >Market Cap Rank
               <p
@@ -88,7 +103,14 @@
               <el-divider id="center-divider" direction="vertical"></el-divider>
             </div>
           </el-col>
-          <el-col :span="11">
+          <el-col
+            style="
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+            "
+            :span="11"
+          >
             <el-col style="text-align: right"
               >Market Cap
               <price-format
@@ -142,5 +164,12 @@ export default {
 #center-divider {
   height: 100% !important;
   margin: auto !important;
+  width: 1px !important;
+  background: #e8e9e9;
+}
+
+#token-metrics .el-divider--horizontal {
+  height: 1px !important;
+  background: #e8e9e9;
 }
 </style>
