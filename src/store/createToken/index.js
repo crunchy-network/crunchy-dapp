1b874d7671e8b652bc3db8eeea512a2f0e053aca
Huy Do <herohthd@gmail.com>
2023-09-03 15:10:06 +0700

    update config

--- src/store/createToken/index.js
+++ src/store/createToken/index.js
@@ -1,14 +1,14 @@
 import actions from "./actions";
 import mutations from "./mutations";
-import fa2 from "./fa2.json";
-import fa12 from "./fa12.json";
+import fa2DefiFixedSupply from "./fa2-defi-fixed-supply.json";
+import fa2DefiMintable from "./fa2-defi-mintable.json";
 
 export default {
   state: {
     loading: false,
     buyBackAndBurnAddress: process.env.VUE_APP_DEX_AGGREGATE_TEZ_DESTINATION,
-    fa12: fa12,
-    fa2: fa2,
+    fa2DefiMintable: fa2DefiMintable,
+    fa2DefiFixedSupply: fa2DefiFixedSupply,
   },
   actions,
   mutations,
