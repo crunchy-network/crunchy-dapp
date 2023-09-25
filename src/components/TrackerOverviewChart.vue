aaa3200a3637868d144a9e16a984c7d30a2e63a6
Huy Do <herohthd@gmail.com>
2023-09-25 15:15:00 +0700

    redo formatNumShortHand

--- src/components/TrackerOverviewChart.vue
+++ src/components/TrackerOverviewChart.vue
@@ -7,7 +7,7 @@
 <script lang="js">
 import { createChart } from "lightweight-charts";
 import { mapGetters } from "vuex";
-// import numberFormat from "../utils/number-format";
+import numberFormat from "../utils/number-format";
 import tokenTracker from "../utils/token-tracker";
 
 export default {
@@ -671,29 +671,7 @@ export default {
     },
 
     formatNumShorthand(value, precision) {
-      const number = precision
-        ? parseFloat(value).toFixed(precision)
-        : Number(value);
-      if (isNaN(number)) return { value: 0, suffix: "" };
-      if (number < 1000) {
-        return { value: number, suffix: "" };
-      } else if (number < 1000000) {
-        const value = number / 1000;
-        const precisedValue = parseFloat(value).toFixed(precision)
-        return { value: precisedValue, suffix: "K" };
-      } else if (number < 1000000000) {
-        const value = number / 1000000;
-        const precisedValue = parseFloat(value).toFixed(precision)
-        return { value: precisedValue, suffix: "M" };
-      } else if (number < 1000000000000) {
-        const value = number / 1000000000;
-        const precisedValue = parseFloat(value).toFixed(precision)
-        return { value: precisedValue, suffix: "B" };
-      } else {
-        const value = number / 1000000000000;
-        const precisedValue = parseFloat(value).toFixed(precision)
-        return { value: precisedValue, suffix: "T" };
-      }
+       return numberFormat.shorthand(value, precision);
     },
   },
 };
