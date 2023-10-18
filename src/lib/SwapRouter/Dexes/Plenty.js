e7158188791fc5e2e21183631239717f623ec0f1
Do Quang Huy <62065172+huydo2105@users.noreply.github.com>
2023-10-18 23:39:13 +0700

    check system fee for plenty pools (#149)

--- src/lib/SwapRouter/Dexes/Plenty.js
+++ src/lib/SwapRouter/Dexes/Plenty.js
@@ -7,11 +7,15 @@ const { getAmmSwapOutput } = require("../SwapRates/amm");
 const { addTokenApprovalOperators } = require("../TokenTypes");
 
 const DEX_FEE = 0.35;
+const SYSTEM_FEE = 1000;
 
 const getSwapOutput = (input, pair) => {
   const inputAfterFee = input * percentToDecimal(DEX_FEE);
   const feeAmount = input - inputAfterFee;
-  if (!isValidDexFee(feeAmount, pair)) {
+  const mutezInput = convertToMuTez(input, pair.a);
+  const systemFee = Math.floor(mutezInput / SYSTEM_FEE);
+
+  if (!isValidDexFee(feeAmount, pair) || systemFee <= 0) {
     return 0;
   }
   return getAmmSwapOutput(inputAfterFee, pair);
@@ -20,6 +24,7 @@ const getSwapOutput = (input, pair) => {
 const buildDexOperation = async (dex, trade, walletAddress, tezos) => {
   const output = convertToMuTez(trade.minOut, trade.b);
   const input = convertToMuTez(trade.input, trade.a);
+
   const transfers = [
     dex.contract.methods
       .Swap(output, walletAddress, trade.b.tokenAddress, trade.b.tokenId, input)
