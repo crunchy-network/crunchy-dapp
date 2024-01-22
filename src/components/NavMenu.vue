aa50b375c701dc0c45669f9223e91e0ae5eb23dd
Josh Dechant <josh@codecrafting.org>
2024-01-22 00:00:31 -0500

    Add staking to nav

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -117,18 +117,19 @@
                 active-class="is-active"
               >
                 <i v-if="mobile" class="fak fa-crunchy-swap-alt"></i>
-
                 <span>Swap</span>
               </router-link>
-              <!-- <router-link
+
+              <router-link
                 tag="li"
                 class="el-menu-item"
                 to="/stake"
                 active-class="is-active"
               >
-                <i v-if="mobile" class="fak fa-crunchy-swap-alt"></i>
+                <i v-if="mobile" class="fak fa-crunchy-staking"></i>
                 <span>Stake</span>
-              </router-link> -->
+              </router-link>
+
               <el-submenu
                 id="defi-menu"
                 :popper-append-to-body="false"
